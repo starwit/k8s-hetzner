2d6b3b74a6680fa027c8d6c099a963865921aa99
automatic push <witchpou@users.noreply.github.com>
2021-10-31 14:41:38 +0100

    added scripts for installations

--- kubernetes-hetzner/variables.tf
+++ kubernetes-hetzner/variables.tf
@@ -1,4 +1,7 @@
 variable "HCLOUD_TOKEN" {}
+
+variable "ssh_public_key" {} # public ssh key without pw to enable tf to run stuff
+variable "ssh_private_key" {} # private ssh key to enable tf...
 variable "node_image" {
   default = "ubuntu-18.04"
 }
